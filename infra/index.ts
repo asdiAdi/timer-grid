96fedabb1f6ae759322c454b2245a3b2e1a7cca5
asdiAdi <carltimothyadi@gmail.com>
2026-09-19 22:28:29 +0800

    feat(infra): add GithubDeployStack and SSM-based deployment workflow

--- infra/index.ts
+++ infra/index.ts
@@ -1,22 +1,46 @@
-import { StaticSiteStack } from "@asdi/aws-infra";
+import { StaticSiteStack, GithubDeployStack } from "@asdi/aws-infra";
 import * as cdk from "aws-cdk-lib";
 
 const app = new cdk.App();
-
-new StaticSiteStack(app, "TimerGridStack", {
+const env = {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT!,
-    region: "us-east-1", //default,
+    region: "us-east-1",
   },
-  staticSite: {
-    secondLevelDomain: "carladi.com",
-    subDomain: "timergrid",
-    github: {
-      owner: "asdiAdi",
-      ownerId: "80302904",
-      repo: "timer-grid",
-      repoId: "1352233635",
-    },
-    tableName: "gh_site_secrets",
+};
+
+const site = new StaticSiteStack(app, "TimerGridSiteStack", {
+  ...env,
+  secondLevelDomain: "carladi.com",
+  subDomain: "timergrid",
+});
+
+const deploy = new GithubDeployStack(app, "TimerGridDeployStack", {
+  ...env,
+  roleName: "timer-grid-github-deploy",
+  github: {
+    owner: "asdiAdi",
+    ownerId: "80302904",
+    repo: "timer-grid",
+    repoId: "1352233635",
+    branch: "main",
   },
+  managedPolicies: [site.staticSite.managedPolicy],
+});
+
+new cdk.CfnOutput(site, "TimerGridBucket", {
+  value: site.staticSite.bucket.bucketName,
+  description: "put to ssm parameter github-action: S3_BUCKET",
+});
+new cdk.CfnOutput(site, "TimerGridDistributionId", {
+  value: site.staticSite.distribution.distributionId,
+  description: "put to ssm parameter github-action: CLOUDFRONT_DISTRIBUTION_ID",
+});
+new cdk.CfnOutput(deploy, "TimerGridRoleToAssume", {
+  value: deploy.role.roleArn,
+  description: "github action variable: AWS_ROLE_TO_ASSUME",
+});
+new cdk.CfnOutput(deploy, "TimerGridRegion", {
+  value: deploy.region,
+  description: "github action variable: AWS_REGION",
 });
